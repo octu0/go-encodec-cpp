210bae61d5837d539c3145e2d205475cf9dcd124
Yusuke Hata <yusuke.hata@mirrativ.co.jp>
2024-01-10 07:57:43 +0000

    copy immediate

--- encodec.go
+++ encodec.go
@@ -14,7 +14,7 @@ import "C"
 import (
 	"errors"
 	"runtime"
-	"sync"
+	"sync/atomic"
 	"unsafe"
 )
 
@@ -26,29 +26,27 @@ var (
 	ErrFetchDecompressAudio = errors.New("failed to cencodec_get_decompress_audio")
 )
 
-type ReleaseFunc func()
-
 type noCopy struct{}
 
 type EncodecContext struct {
 	noCopy noCopy
 
-	ptr  unsafe.Pointer
-	once sync.Once
+	ptr unsafe.Pointer
+	r   int32
 }
 
 func (e *EncodecContext) Release() {
-	e.once.Do(func() {
+	if atomic.CompareAndSwapInt32(&e.r, 0, 1) {
 		runtime.SetFinalizer(e, nil)
 		C.cencodec_free(e.ptr)
-	})
+	}
 }
 
 func (e *EncodecContext) SetTargetBandwidth(bandwidth int) {
 	C.cencodec_set_target_bandwidth(e.ptr, C.int(bandwidth))
 }
 
-func (e *EncodecContext) CompressAudio(audio []float32, nThreads int) ([]int32, ReleaseFunc, error) {
+func (e *EncodecContext) CompressAudio(audio []float32, nThreads int) ([]int32, error) {
 	ret := C.cencodec_compress_audio(
 		e.ptr,
 		(*C.float)(unsafe.Pointer(&audio[0])),
@@ -56,23 +54,22 @@ func (e *EncodecContext) CompressAudio(audio []float32, nThreads int) ([]int32, 
 		C.int(nThreads),
 	)
 	if ret != C.int(0) {
-		return nil, nil, ErrCompressAudio
+		return nil, ErrCompressAudio
 	}
 
 	data := C.cencodec_get_compress_codes(e.ptr)
 	if data == nil {
-		return nil, nil, ErrFetchCompressAudio
+		return nil, ErrFetchCompressAudio
 	}
 
-	free := func() {
-		C.cencodec_compressed_free(data)
-	}
-	once := new(sync.Once)
-	codes := unsafe.Slice((*int32)(unsafe.Pointer(data.codes)), int(data.codes_len))
-	return codes, func() { once.Do(free) }, nil
+	tmp := unsafe.Slice((*int32)(unsafe.Pointer(data.codes)), int(data.codes_len))
+	codes := make([]int32, int(data.codes_len))
+	copy(codes, tmp)
+	C.cencodec_compressed_free(data)
+	return codes, nil
 }
 
-func (e *EncodecContext) DecompressAudio(codes []int32, nThreads int) ([]float32, ReleaseFunc, error) {
+func (e *EncodecContext) DecompressAudio(codes []int32, nThreads int) ([]float32, error) {
 	ret := C.cencodec_decompress_audio(
 		e.ptr,
 		(*C.int32_t)(unsafe.Pointer(&codes[0])),
@@ -80,27 +77,26 @@ func (e *EncodecContext) DecompressAudio(codes []int32, nThreads int) ([]float32
 		C.int(nThreads),
 	)
 	if ret != C.int(0) {
-		return nil, nil, ErrDecompressAudio
+		return nil, ErrDecompressAudio
 	}
 
 	data := C.cencodec_get_decompress_audio(e.ptr)
 	if data == nil {
-		return nil, nil, ErrFetchDecompressAudio
+		return nil, ErrFetchDecompressAudio
 	}
 
-	free := func() {
-		C.cencodec_decompressed_free(data)
-	}
-	once := new(sync.Once)
-	audio := unsafe.Slice((*float32)(unsafe.Pointer(data.audio)), int(data.audio_len))
-	return audio, func() { once.Do(free) }, nil
+	tmp := unsafe.Slice((*float32)(unsafe.Pointer(data.audio)), int(data.audio_len))
+	audio := make([]float32, int(data.audio_len))
+	copy(audio, tmp)
+	C.cencodec_decompressed_free(data)
+	return audio, nil
 }
 
 func LoadModel(path string, nGPULayers int) (*EncodecContext, error) {
 	cs := C.CString(path)
 	defer C.free(unsafe.Pointer(cs))
 
-	ptr := unsafe.Pointer(C.cencodec_load_model(cs, C.int(nGPULayers)))
+	ptr := C.cencodec_load_model(cs, C.int(nGPULayers))
 	if ptr == nil {
 		return nil, ErrLoadModel
 	}
@@ -108,7 +104,7 @@ func LoadModel(path string, nGPULayers int) (*EncodecContext, error) {
 	ctx := &EncodecContext{
 		noCopy: noCopy{},
 		ptr:    ptr,
-		once:   sync.Once{},
+		r:      0,
 	}
 	runtime.SetFinalizer(ctx, finalizeEncodeContext)
 	return ctx, nil
